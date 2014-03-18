dd05bd8630da62c4367a888da8a3545b87be211a
doug <doug@localhost.localdomain>
2014-03-18 16:05:24 -0400

    Adds a #define feature to the preprocessor

--- ST2-syntax/copier.sh
+++ ST2-syntax/copier.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp ethereumcll.syntax.plist /home/doug/.config/sublime-text-2/Packages/EthereumCLL/EthereumCLL.tmLanguage
+cp ethereumcll.syntax.plist $HOME/.config/sublime-text-2/Packages/EthereumCLL/EthereumCLL.tmLanguage
--- prepiler.php
+++ prepiler.php
@@ -10,18 +10,24 @@ class prepiler {
 
 		$file = file_get_contents($argv[1]);
 
+		$file = $this->processDefines($file);
+
 		$result = "";
 		$lines = preg_split("/\n/", $file);
 		foreach ($lines as $l) {
+
 			$trimmer = trim($l);
+
 			// Strip blanks
 			if (!strlen($trimmer)) {
 				continue;
 			}
+
 			// Ignore comments.
 			if (preg_match("/^\/\//", $trimmer)) {
 				continue;
 			}
+
 			// Stop at multi-equal
 			if (preg_match("/^\=\=/", $trimmer)) {
 				break;
@@ -30,6 +36,53 @@ class prepiler {
 		}
 
 		file_put_contents($this->outfile, $result);
+
+	}
+
+	function processDefines($file) {
+
+		$defines = array();
+
+		$result = "";
+		$lines = preg_split("/\n/", $file);
+		$linenumber = 0;
+		foreach ($lines as $l) {
+			$linenumber++;
+			$trimmer = trim($l);
+
+			// Collect each define
+			if (preg_match("/^\#define/", $trimmer)) {
+				// Great, it's a define. Now let's make sure it has all it's parts.
+				if (preg_match("/^\#define\s+[^\s]+\s[^\s]+$/",$trimmer)) {
+					// Great that has all it's parts.
+					$variable_name = preg_replace("/^\#define\s+([^\s]+)\s+[^\s]+$/", "$1", $trimmer);
+					$variable_value = preg_replace("/^\#define\s+[^\s]+\s+([^\s]+)$/", "$1", $trimmer);
+					
+					if (!preg_match("/^[A-Z_]+$/", $variable_name)) {
+						echo "WARNING: Your define of '".$variable_name."' isn't all uppercase or underscores. Which is OK, but, I don't recommend it.\n";
+					}
+
+					$defines[$variable_name] = $variable_value;
+
+				} else {
+					echo "In line number ".$linenumber." with the define statement of '".$trimmer."' I can't parse it properly. Sorry\n";
+					die();
+				}
+			} else {
+				$result .= $l."\n";
+			}
+		}
+
+		// Now, go and replace the result.
+		foreach ($defines as $def => $val) {
+			$result = preg_replace("/".$def."/", $val, $result);
+		}
+
+		// print_r($defines);
+
+		// echo $result."\n\n";
+
+		return $result;
 
 	}
 
