b83332fba7419a2753022cfec3e62c5e410cd9ab
dougbtv <douglaskippsmith@gmail.com>
2014-03-27 11:19:12 -0400

    Improves the compile.sh with arguments

--- compile.sh
+++ compile.sh
@@ -1,10 +1,62 @@
 #!/bin/bash
+
+usage()
+{
+cat << EOF
+usage: $0 inputfile.cll {options}
+
+This script run the test1 or test2 over a machine.
+
+OPTIONS:
+   -h      
+   		Show this message
+   
+   -i      
+   		Enable inspection (shows assembled output, line-by-line)
+   
+   -t "regex"
+   		Used with -i. Trace lines in the assembled piece using "regex".
+
+EOF
+}
+
+DO_INSPECT=0
+DO_TRACE=0
+TRACE_REGEX=
+# This skips the first argument, so we can use it as the input file name (always required)
+while getopts “hit:” OPTION ${@:2}; 
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         i)
+             DO_INSPECT=1
+             ;;
+         t)
+		 	 DO_TRACE=1
+             TRACE_REGEX=$OPTARG
+             ;;
+         ?)
+			 usage
+             exit
+             ;;
+     esac
+done
+
 TEMP_FILE=/tmp/inspect.asm
 echo $1
 if [[ -e $1 ]]; then
 	php -q cllPreProcessor.php $1
 	eval "python docompile.py | tee $TEMP_FILE"
-	# php -q postInspection.php $TEMP_FILE
+	if [ $DO_INSPECT == 1 ]; then
+		if [ $DO_TRACE == 1]; then
+			php -q postInspection.php $TEMP_FILE "$TRACE_REGEX"
+		else
+			php -q postInspection.php $TEMP_FILE
+		fi
+	fi
 else
 	echo "The file $1 doesn't exist. Pass in your file.cll as the first argument."
 fi
--- postInspection.php
+++ postInspection.php
@@ -6,17 +6,28 @@
 	class postInspection {
 
 		var $output_pattern = "Output: ";
+		var $pad_amount = 10;
+		var $do_trace = false;
+		var $trace_pattern = "";
 
 		function postInspection($argv) {
 
 			echo "\n\n";
 			echo "------------------------------ -\n";
-			echo "-- EVM-ASM inspection ------- -\n";
+			echo "-- EVM3-ASM inspection ------ -\n";
 			echo "---------------------------- -\n";
 
 			$inspectfile = $argv[1];
 			if (!file_exists($inspectfile)) {
 				die("postInspection: Sorry, I don't know the file: ".$inspectfile);
+			}
+
+			if (isset($argv[2])) {
+				if (strlen($argv[2])) {
+					// That's our pattern for tracing.
+					$this->do_trace = true;
+					$this->trace_pattern = "/".$argv[2]."/";
+				}
 			}
 			
 			// Grep the part of the file that we want.
@@ -32,7 +43,21 @@
 			$idx = 0;
 			foreach ($bytes as $b) {
 				$idx++;
-				echo sprintf("%04d",$idx)."\t".$b."\n";
+				$instruction = $b;
+				if ($this->do_trace) {
+					// echo $this->trace_pattern." | ".$instruction."\n\n";
+
+					if (preg_match($this->trace_pattern, $instruction)) {
+						// Ok, we're tracing that.
+						// So, let's figure out how much to pad it.
+						$pad = $this->pad_amount - strlen($instruction);
+						for ($i = 0; $i < $pad; $i++) {
+							$instruction .= " ";
+						}
+						$instruction .= " <-------";
+					}
+				}
+				echo sprintf("%04d",$idx)."\t".$instruction."\n";
 			}
 
 			echo "\n\n";
